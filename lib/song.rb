a30c55a18d10e209a07feb3df5e65a4b1359d985
David Foote <david.a.foote@gmail.com>
2019-06-24 16:50:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,8 @@ class Song
   
   def self.new_from_filename(filename)
     song = self.new
-    song.name = filename.split(" - ")[0].split(" . ")[1]
+    song.name = filename.split(" - ")[1].split(" . ")[0]
+    song.artist_name = 
   end
   
   def self.create_from_filename(filename)
